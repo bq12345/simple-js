aed18cfeff9cf8d3bbf77c0ebcd25a847bcd8ef2
bq12345 <1075707996@qq.com>
2014-04-08 13:05:27 +0800

    I add some css3 effects
    Thanks for your reading...

---
+++ WebContent/css3/data.js
@@ -0,0 +1,11 @@
+var images=[
+            'images/012.jpg',
+            'images/013.jpg',
+            'images/014.jpg',
+            'images/015.jpg',
+            'images/016.jpg',
+            'images/017.jpg',
+            'images/018.jpg',
+            'images/019.jpg',
+            'images/020.jpg',
+            ];
